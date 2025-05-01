231eebe768607f229511a380bb1d558c72b92f76
hemarhip <hema.rhip@gmail.com>
2025-05-01 12:56:33 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,7 @@
+#Task 2: Using the Math Module for Calculations
+
+n = int(input('Enter a number: '))
+from math import *
+print('Square Root:',sqrt(n))
+print('Logarithm:',log(n))
+print('Sine:',sin(n))
